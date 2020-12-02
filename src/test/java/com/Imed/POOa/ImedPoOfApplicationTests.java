58fd2c1f9f6ae243ec8529c8f10cbbde83c7d6b5
Jordan Mucha <Mucha.J@hotmail.com>
2020-12-02 12:18:53 -0300

    Comentario - Iniciando projeto

---
+++ src/test/java/com/Imed/POOa/ImedPoOfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Imed.POOa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImedPoOfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
